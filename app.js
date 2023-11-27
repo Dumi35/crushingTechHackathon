67f2aca5c1c6fe56e97324104319e2ecf44e323a
Dumebi35 <71632162+Dumi35@users.noreply.github.com>
2023-11-28 00:37:00 +0100

    Checkmark Animation color

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ let bell = document.getElementsByClassName("bell")[0]
 let alert = document.getElementsByClassName("alert")[0]
 let profile = document.getElementsByClassName("profile")[0]
 let dropdown = document.getElementsByClassName("dropdown")[0]
-let dismissTrial = document.getElementsByClassName("controls")[0]
-let hideSteps = document.getElementsByClassName("controls")[1]
+let dismissTrial = document.getElementsByClassName("dismiss")[0]
+let hideSteps = document.getElementsByClassName("controls")[0]
 let extendTrial = document.getElementsByClassName("extend")[0]
 let allSteps = document.getElementsByClassName("allSteps")[0]
 
@@ -54,11 +54,11 @@ steps.forEach(function(step) {
         
         document.querySelectorAll('details').forEach(function(otherDetails) {
            
-           /*  otherDetails.setAttribute("open","true") */
             
             if (otherDetails !== step) {
                 otherDetails.removeAttribute('open');
             }  
+
             
         });
     });
@@ -70,7 +70,7 @@ let completionStatement = document.getElementById("completion-statement")
 let stepSelect = document.querySelectorAll('.step-select')
 let progress = document.getElementsByClassName("progress-bar-complete")[0]
 let completed=0;
-completionStatement.innerText= `${completed}/5 completed`
+completionStatement.innerText= `${completed} / 5 completed`
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -81,7 +81,7 @@ async function svgLoadingDelay(selection) {
     await sleep(400);
 
     selection.innerHTML = `<svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="12" cy="12" r="10" fill="#303030"></circle>
+        <circle cx="12" cy="12" r="10" fill="#1C181D"></circle>
         <path
         d="M17.2738 8.52629C17.6643 8.91682 17.6643 9.54998 17.2738 9.94051L11.4405 15.7738C11.05 16.1644 10.4168 16.1644 10.0263 15.7738L7.3596 13.1072C6.96908 12.7166 6.96908 12.0835 7.3596 11.693C7.75013 11.3024 8.38329 11.3024 8.77382 11.693L10.7334 13.6525L15.8596 8.52629C16.2501 8.13577 16.8833 8.13577 17.2738 8.52629Z"
         fill="#fff"
@@ -103,14 +103,14 @@ stepSelect.forEach((selection)=>{
                 progress.style.borderBottomRightRadius="5px"
             } 
 
-            completionStatement.innerText= `${completed}/5 completed`
+            completionStatement.innerText= `${completed} / 5 completed`
             progress.style.width=`${(completed/5)*100}%`
 
             /* change svg pic */
             selection.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 30 30" fill="none">
             <!-- Initial shape: Original SVG path with rotation -->
             <path id="originalPath" d="M26 14C26 16.3734 25.2962 18.6935 23.9776 20.6668C22.6591 22.6402 20.7849 24.1783 18.5922 25.0866C16.3995 25.9948 13.9867 26.2324 11.6589 25.7694C9.33114 25.3064 7.19295 24.1635 5.51472 22.4853C3.83649 20.8071 2.6936 18.6689 2.23058 16.3411C1.76755 14.0133 2.00519 11.6005 2.91345 9.4078C3.8217 7.21509 5.35977 5.34094 7.33316 4.02236C9.30655 2.70379 11.6266 2 14 2"
-              stroke="#000" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+              stroke="#1C181D" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
                 <animateTransform attributeName="transform" type="rotate" from="0 14 14" to="360 14 14" dur="2s" repeatCount="indefinite" />
                 <animate attributeName="d" values="M26 14C26 16.3734 25.2962 18.6935 23.9776 20.6668C22.6591 22.6402 20.7849 24.1783 18.5922 25.0866C16.3995 25.9948 13.9867 26.2324 11.6589 25.7694C9.33114 25.3064 7.19295 24.1635 5.51472 22.4853C3.83649 20.8071 2.6936 18.6689 2.23058 16.3411C1.76755 14.0133 2.00519 11.6005 2.91345 9.4078C3.8217 7.21509 5.35977 5.34094 7.33316 4.02236C9.30655 2.70379 11.6266 2 14 2" dur="2s" repeatCount="1" />
             </path>
@@ -123,7 +123,7 @@ stepSelect.forEach((selection)=>{
             
             completed-=1
             
-            completionStatement.innerText= `${completed}/5 completed`
+            completionStatement.innerText= `${completed} / 5 completed`
             progress.style.width=`${(completed/5)*100}%`
             progress.style.borderTopRightRadius="0px"
             progress.style.borderBottomRightRadius="0px"
