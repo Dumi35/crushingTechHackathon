eeffaebf0162fe99e22bdabd6d86216b078cb86b
Dumebi35 <71632162+Dumi35@users.noreply.github.com>
2024-01-12 13:45:55 +0100

    Design References

--- app.js
+++ app.js
@@ -92,11 +92,13 @@ async function svgLoadingDelay(selection) {
      
 }
 
+
+
 stepSelect.forEach((selection)=>{
     let isSelected = false;
-    
     selection.addEventListener("click",()=>{
         selection.parentElement.parentElement.parentElement.removeAttribute("open")
+
 
         isSelected = !isSelected
         if(isSelected){
@@ -110,7 +112,7 @@ stepSelect.forEach((selection)=>{
             completionStatement.innerText= `${completed} / 5 completed`
             progress.style.width=`${(completed/5)*100}%`
 
-            /* change svg pic to loading animation */
+            //change svg pic to loading animation 
             selection.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 30 30" fill="none">
             <!-- Initial shape: Original SVG path with rotation -->
             <path id="originalPath" d="M26 14C26 16.3734 25.2962 18.6935 23.9776 20.6668C22.6591 22.6402 20.7849 24.1783 18.5922 25.0866C16.3995 25.9948 13.9867 26.2324 11.6589 25.7694C9.33114 25.3064 7.19295 24.1635 5.51472 22.4853C3.83649 20.8071 2.6936 18.6689 2.23058 16.3411C1.76755 14.0133 2.00519 11.6005 2.91345 9.4078C3.8217 7.21509 5.35977 5.34094 7.33316 4.02236C9.30655 2.70379 11.6266 2 14 2"
@@ -138,10 +140,8 @@ stepSelect.forEach((selection)=>{
         }
         
         
-    })
-
-    
-}) 
+    })  
+})  
 
 /* close extend trial bar */
 dismissTrial.addEventListener("click",()=>{
